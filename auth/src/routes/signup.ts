8bc7bf4cdfdea32b2472847ee349b09521783c0f
fallout0128 <68479312+fallout0128@users.noreply.github.com>
2020-11-09 22:06:12 +0300

    jwt added. checkpoint

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
+import jwt from 'jsonwebtoken'
 import { User } from '../models/user'
 import { RequestValidationError } from '../errors/request-validation-error'
 import { BadRequestError } from '../errors/bad-request-error'
@@ -27,7 +28,22 @@ router.post('/api/users/signup', rules, async (req: Request, res: Response) => {
   }
 
   const user = User.build({ email, password })
+  
   await user.save()
+
+  //generate JWT
+  const userJwt = jwt.sign(
+    {
+      id: user.id,
+      email: user.email
+    }, 
+    process.env.JWT_KEY!
+  )
+
+  //store it on session object
+  req.session = { 
+    jwt: userJwt
+  }
 
   res.status(201).send(user)
 })
