f62462a6604e5753c71b69ff40ed3d638d7ded64
fallout0128 <68479312+fallout0128@users.noreply.github.com>
2020-11-16 18:51:07 +0300

    client is done! checkpoint

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,22 +1,13 @@
-import axios from 'axios'
+import buildClient from '../api/build-client'
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser)
-  return <h1>lending page</h1>
+  return currentUser? 
+    <h1>You are sign in</h1> : <h1>You are not sign in</h1> 
 }
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    //we are on the server
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      { headers: req.headers }
-    )
-    return data
-  }
-
-  //we are on the browser
-  const { data } = await axios.get('/api/users/currentuser')
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context)
+  const { data } = await client.get('/api/users/currentuser')
   return data
 }
 
